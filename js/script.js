f933428189aa7b3ef3113952a7345735585fdd83
Pavel Podlesnyi <pasha011995@gmail.com>
2021-05-11 13:49:36 +0300

    add border and localstorage

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
 'use strict';
 
 const result = document.querySelector('.calculating__result span');
-let sex = 'female',
-    height, weight, age, 
-    ratio = 1.375;
+
+let sex, height, weight, age, ratio;
+
+if (localStorage.getItem('sex')) {
+  sex = localStorage.getItem('sex');
+} else {
+  sex = 'female';
+  localStorage.setItem('sex', 'female');
+}
+
+if (localStorage.getItem('ratio')) {
+  ratio = localStorage.getItem('ratio');
+} else {
+  ratio = 1.375;
+  localStorage.setItem('ratio', 1.375);
+}
+
+function setStaticElement(selector, activeClass) {
+  const elements = document.querySelectorAll(selector);
+  elements.forEach(elem => {
+    elem.classList.remove(activeClass);
+    console.log(elem);
+    if ( elem.getAttribute('id') === localStorage.getItem('sex') ) {
+      elem.classList.add(activeClass);
+    }
+
+    if ( elem.getAttribute('data-ratio') === localStorage.getItem('ratio') ) {
+      elem.classList.add(activeClass);
+    }
+  });
+}
+
+setStaticElement('#gender div', 'calculating__choose-item_active');
+setStaticElement('.calculating__choose_big div', 'calculating__choose-item_active');
 
 function setResult() {
   if (sex && height && weight && age && ratio) {
@@ -31,8 +62,10 @@ function getStaticElement(parent) {
 
       if (parent === '.calculating__choose_big') {
         ratio = elem.getAttribute('data-ratio');
+        localStorage.setItem('ratio', elem.getAttribute('data-ratio'));
       } else {
         sex = elem.getAttribute('id');
+        localStorage.setItem('sex', elem.getAttribute('id'));
       }
 
       setResult(); 
@@ -46,6 +79,13 @@ getStaticElement('.calculating__choose_big');
 function getDynamicElement(elem) {
   const input = document.querySelector(elem);
   input.addEventListener('input', () => {
+
+    if (input.value.match(/\D/g)) {
+      input.style.border = '1px solid red';
+    } else {
+      input.style.border = 'none';
+    }
+
     switch (input.getAttribute('id')) {
       case 'height':
         height = +input.value;
